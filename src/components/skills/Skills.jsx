2d35f72d87bfc2d164b07f1b0f698df6248096ae
AvinavStha <avi.shrestha.775@gmail.com>
2022-02-08 11:29:17 +0545

    sUpdate

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,35 +6,36 @@ const Skills = () => {
     return (
         <div className="s">
             <div className="s-left">
-                <div className="s-background"></div>
-                <div className="s-left-wrapper">
+                <div className="s-background">
+                    <div className="s-left-wrapper">
 
-                    <div className="s-texts">
-                        <h1 className="s-title">
-                            My Skills
-                        </h1>
-                        <p className="s-desc">
-                        I've basically obtained a decent knowledge of computer science and web development ideas as a learner, and I've spent a lot of my time applying these concepts to real-world applications.
-                        </p>
-                    </div>
-                    <div className="s-Type">
-                        <span className="s-types">
-                        &#8226; HTML & CSS
+                        <div className="s-texts">
+                            <h1 className="s-title">
+                                My Skills
+                            </h1>
+                            <p className="s-desc">
+                                I've basically obtained a decent knowledge of computer science and web development ideas as a learner, and I've spent a lot of my time applying these concepts to real-world applications.
+                            </p>
+                        </div>
+                        <div className="s-Type">
+                            <span className="s-types">
+                                &#8226; HTML & CSS
                             </span >
-                        <span className="s-types"> &#8226; Angular JS Basic</span >
-                        <span className="s-types">&#8226; Python (Django)</span >
-                        <span className="s-types">&#8226; PHP</span>
-                        <span className="s-types">&#8226; Java</span>
-                        <span className="s-types">&#8226; React JS Basic</span>
+                            <span className="s-types"> &#8226; Angular JS Basic</span >
+                            <span className="s-types">&#8226; Python (Django)</span >
+                            <span className="s-types">&#8226; PHP</span>
+                            <span className="s-types">&#8226; Java</span>
+                            <span className="s-types">&#8226; React JS Basic</span>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="s-right">
-                
+
                 <div className="s-img">
                     <CarouselContainer />
                 </div>
-                
+
             </div>
         </div>
     );

40dc68c41131e3ae19901686238a6713f3f1146e
AvinavStha <avi.shrestha.775@gmail.com>
2022-07-20 22:17:36 +0545

    added-projects

--- src/data.js
+++ src/data.js
@@ -1,19 +1,22 @@
 export const products = [
-    {
-        id: 1,
-        img:"https://i.ibb.co/MGHFnBd/deal-With-Us-Express.png",
-        link:"",
-
-    },
-    {
-        id: 2,
-        img :"https://i.ibb.co/JQpJ55K/admin-Dashboard.png",
-        link:"",
-
-    },
-    {
-        id: 3,
-        img:"https://i.ibb.co/ZT4fc86/portfolio-Demo.png",
-        link:"",
-    },
-]
+  {
+    id: 1,
+    img: "https://i.ibb.co/MGHFnBd/deal-With-Us-Express.png",
+    link: "",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/JQpJ55K/admin-Dashboard.png",
+    link: "",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/ZT4fc86/portfolio-Demo.png",
+    link: "https://avinavstha.github.io/AviPortfolio/",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/HT2kV11/screenshot.png",
+    link: "https://fitclub-dnt.pages.dev/",
+  },
+];
